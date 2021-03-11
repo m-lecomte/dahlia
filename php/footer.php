050d0e0ce8ea2991fa7462481240d92cc8e854ca
Noctambule <aureliepinna96@gmail.com>
2021-03-11 12:56:09 +0100

    Adding footer and header 2

--- php/footer.php
+++ php/footer.php
@@ -1,5 +1,10 @@
 <footer>
 
-
+    <nav id="footersNav">
+        <ul>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
 
 </footer>
--- php/header.php
+++ php/header.php
@@ -1,5 +1,13 @@
 <header>
+    <a href="index.php">
+        <img src="./../img/" alt="Dahlia's logo" />
+    </a>
 
-
+    <nav id="mainNav">
+        <ul>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
 
 </header>
