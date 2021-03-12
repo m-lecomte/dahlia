8c643e8eefaaf043b434eebae01bf9467eb4fc60
m-lecomte <76045569+m-lecomte@users.noreply.github.com>
2021-03-12 17:00:35 +0100

    PHP starting code

--- index.php
+++ index.php
@@ -7,50 +7,58 @@
 <body>
     <?php
 
-    $mysqli = new mysqli("localhost", "me", "password", "quizz");
+    $mysqli = new mysqli("localhost", "me", "password", "dahlia");
 
     if ($mysqli->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli->connect_error;
         exit();
     }
 
-    $sql = "SELECT * FROM question WHERE id=1";
+    $sql = "SELECT * FROM chat";
     $result = $mysqli->query($sql);
 
     // Associative array
     $row = $result->fetch_assoc();
-    printf("%s <br>%s\n", $row["énoncé"], $row["r3"]);
+    printf("%s <br>%s\n", $row["author"], $row["title"]);
 
     // Free result set
     $result->free_result();
 
-    // function to send message
-    /*function send_message($message)
-    {
 
+    if (isset($_POST["message"])) {
+        echo "posted !";
+        $message = $_POST["message"];
         //$mysqli = new mysqli("localhost","me","password","quizz");
-
-        // Turn autocommit off
         $mysqli->autocommit(FALSE);
 
-        // Insert some values
-        $mysqli->query("INSERT INTO Persons (FirstName,LastName,Age)
-VALUES ('Peter','Griffin',35)");
-        $mysqli->query("INSERT INTO Persons (FirstName,LastName,Age)
-VALUES ('Glenn','Quagmire',33)");
+        // post message
+        $mysqli->query("INSERT INTO message (author, text, id_chat) VALUES ('1', '" . $message . "', '1')");
+        //$mysqli->query("INSERT INTO test (test2) VALUES ('" . $message . "')");
 
         // Commit transaction
         if (!$mysqli->commit()) {
             echo "Commit transaction failed";
             exit();
+        } else {
+
+            echo "<br>commit<br>";
+
+            $sql2 = "SELECT * FROM message";
+            //$sql2 = "SELECT * FROM test";
+            $result2 = $mysqli->query($sql2);
+
+            // print commit content
+            $row2 = $result2->fetch_assoc();
+            printf("%s | %s\n", $row2["author"], $row2["text"]);
+            //printf("%s | %s", $row2["test1"], $row2["test2"]);
         }
-    }*/
+    }
 
-    $mysqli->close();
+
     ?>
-    <form action="index.php" method="POST">
-
-        <input type="submit" value="send">
+    <form action='index.php' method='POST'>
+        <label for="message"><input type="text" id="message" name="message"></label>
+        <input type='submit' value='send'>
     </form>
     <?php
     $mysqli->close();
