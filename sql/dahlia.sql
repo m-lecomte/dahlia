95c607420dbec180e1369a095255076928aaef84
m-lecomte <76045569+m-lecomte@users.noreply.github.com>
2021-03-15 21:46:19 +0100

    new database version. No foreign keys

--- sql/dahlia.sql
+++ sql/dahlia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : jeu. 11 mars 2021 à 21:22
+-- Généré le : lun. 15 mars 2021 à 21:44
 -- Version du serveur :  10.3.27-MariaDB-0+deb10u1
 -- Version de PHP : 7.3.19-1~deb10u1
 
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `add_user_as`
+--
+
+CREATE TABLE `add_user_as` (
+  `circle` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  `user_added` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `agenda`
+--
+
+CREATE TABLE `agenda` (
+  `owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `chat`
 --
 
@@ -32,7 +54,7 @@ CREATE TABLE `chat` (
   `title` varchar(64) NOT NULL,
   `author` int(11) NOT NULL,
   `date_creation` timestamp NOT NULL DEFAULT current_timestamp(),
-  `circle` set('Family','Friends','Aquaintance','Skills') NOT NULL DEFAULT 'Skills',
+  `personal_circle` int(11) NOT NULL,
   `heading` varchar(64) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -40,9 +62,58 @@ CREATE TABLE `chat` (
 -- Déchargement des données de la table `chat`
 --
 
-INSERT INTO `chat` (`id`, `title`, `author`, `date_creation`, `circle`, `heading`) VALUES
-(1, 'linux', 1, '2021-03-11 18:40:24', 'Skills', NULL),
-(2, 'linux', 1, '2021-03-11 18:40:30', 'Skills', NULL);
+INSERT INTO `chat` (`id`, `title`, `author`, `date_creation`, `personal_circle`, `heading`) VALUES
+(1, 'linux', 1, '2021-03-11 18:40:24', 8, NULL),
+(2, 'linux', 1, '2021-03-11 18:40:30', 8, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `event`
+--
+
+CREATE TABLE `event` (
+  `agenda` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `description` varchar(500) NOT NULL,
+  `type` enum('private','shared','public') NOT NULL DEFAULT 'private'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `file`
+--
+
+CREATE TABLE `file` (
+  `id` int(11) NOT NULL,
+  `data` mediumint(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `heading`
+--
+
+CREATE TABLE `heading` (
+  `id` int(11) NOT NULL,
+  `author` int(11) NOT NULL,
+  `date_creation` timestamp NOT NULL DEFAULT current_timestamp(),
+  `title` varchar(64) NOT NULL,
+  `public_circle` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `member`
+--
+
+CREATE TABLE `member` (
+  `user` int(11) NOT NULL,
+  `chat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -56,16 +127,38 @@ CREATE TABLE `message` (
   `text` varchar(400) NOT NULL,
   `picture_address` varchar(128) DEFAULT NULL,
   `event` int(11) DEFAULT NULL,
-  `id_chat` int(11) NOT NULL
+  `chat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `message`
 --
 
-INSERT INTO `message` (`author`, `date`, `text`, `picture_address`, `event`, `id_chat`) VALUES
+INSERT INTO `message` (`author`, `date`, `text`, `picture_address`, `event`, `chat`) VALUES
 (1, '2021-03-11 20:08:38', '', NULL, NULL, 1),
 (1, '2021-03-11 20:08:47', 'test3', NULL, NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personal_circle`
+--
+
+CREATE TABLE `personal_circle` (
+  `id` int(11) NOT NULL,
+  `title` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `public_circle`
+--
+
+CREATE TABLE `public_circle` (
+  `id` int(11) NOT NULL,
+  `title` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -88,9 +181,47 @@ INSERT INTO `test` (`test1`, `test2`) VALUES
 (3, 'test3'),
 (4, '');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(64) DEFAULT NULL,
+  `email_address` varchar(64) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `gender` enum('M','F','Other') DEFAULT NULL,
+  `language` enum('en','fr','vn') NOT NULL,
+  `birth` date DEFAULT NULL,
+  `geo_location` point DEFAULT NULL,
+  `picture` int(11) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `visible_in`
+--
+
+CREATE TABLE `visible_in` (
+  `circle` int(11) NOT NULL,
+  `event` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `agenda`
+--
+ALTER TABLE `agenda`
+  ADD PRIMARY KEY (`owner`);
 
 --
 -- Index pour la table `chat`
@@ -99,17 +230,58 @@ ALTER TABLE `chat`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`agenda`,`date`);
+
+--
+-- Index pour la table `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `heading`
+--
+ALTER TABLE `heading`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `author_id` (`author`),
+  ADD KEY `circle_id` (`public_circle`);
+
+--
 -- Index pour la table `message`
 --
 ALTER TABLE `message`
   ADD PRIMARY KEY (`author`,`date`),
-  ADD KEY `id_chat` (`id_chat`);
+  ADD KEY `chat_id` (`chat`);
+
+--
+-- Index pour la table `personal_circle`
+--
+ALTER TABLE `personal_circle`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `public_circle`
+--
+ALTER TABLE `public_circle`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- Index pour la table `test`
 --
 ALTER TABLE `test`
   ADD PRIMARY KEY (`test1`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id` (`user_id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -122,20 +294,45 @@ ALTER TABLE `chat`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT pour la table `file`
+--
+ALTER TABLE `file`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `heading`
+--
+ALTER TABLE `heading`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `message`
+--
+ALTER TABLE `message`
+  MODIFY `author` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT pour la table `test`
 --
 ALTER TABLE `test`
   MODIFY `test1` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `message`
+-- Contraintes pour la table `heading`
 --
-ALTER TABLE `message`
-  ADD CONSTRAINT `id_chat` FOREIGN KEY (`id_chat`) REFERENCES `chat` (`id`);
+ALTER TABLE `heading`
+  ADD CONSTRAINT `author_id` FOREIGN KEY (`author`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `circle_id` FOREIGN KEY (`public_circle`) REFERENCES `public_circle` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
