326e8e83d4ab4281bf0326b479946adfe2b4f34b
Arthur <rault.arthur7@gmail.com>
2021-03-16 18:02:30 +0100

    refactor things

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="img/Dahlia_Logo_gradient_icon.ico">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/loginRegister.css">
     <script src="https://kit.fontawesome.com/3297aaa2b8.js" crossorigin="anonymous"></script>
     <title>Dahlia - Login</title>
 </head>
--- register.php
+++ register.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="img/Dahlia_Logo_gradient_icon.ico">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/loginRegister.css">
     <script src="https://kit.fontawesome.com/3297aaa2b8.js" crossorigin="anonymous"></script>
-    <title>Dahlia - Login</title>
+    <title>Dahlia - Register</title>
 </head>
 <body>
 <?php
@@ -19,7 +19,6 @@ include 'php/header.inc.php';
         <input type="text" id="fname" name="firstname" placeholder="Mail Address">
         <input type="password" id="lname" name="lastname" placeholder="Password">
         <input type="submit" value="Log In">
-        <p class="smallText">Does not have an account yet ? Register <a href="register.php>">here</a> !</p>
     </form>
 </div>
 <?php
